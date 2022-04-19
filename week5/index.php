193f54239ce258b4d08189c48b400fd63d4cc5e7
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-04-19 17:41:06 +0000

    the first commit

---
+++ week5/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+$myname = "nacer kraa"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title <?php echo "NacerKraaPHP" ?>>NacerKraaPHP</title>
+</head>
+<body>
+    <h1>Nacer Kraa</h1>
+    <pre>ASCII ART:
+
+        ***    **
+        ****   **
+        ** **  **
+        **  ** **
+        **   ****
+        **    ***
+
+    </pre>
+
+</body>
+</html>
